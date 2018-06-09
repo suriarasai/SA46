a77a026addd1af938412b2c3f6ad7937f88c499f
suriarasai <suria.r.asai@gmail.com>
2018-06-09 20:18:28 +0800

    Student

---
+++ StudentSpringDemo/src/main/java/sg/iss/demo/services/StudentServiceImpl.java
@@ -0,0 +1,64 @@
+package sg.iss.demo.services;
+
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import sg.iss.demo.model.Student;
+import sg.iss.demo.repo.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	@Resource
+	StudentRepository srepo;
+	
+	
+
+	@Override
+	@Transactional
+	public ArrayList<Student> findAllStudents() {
+		return (ArrayList<Student>) srepo.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Student findStudent(String nric) {
+		System.out.println("NRIC"+nric);
+		Student student = srepo.findOne(nric);
+		System.out.println(student.toString());
+		return student;
+
+	}
+
+	@Override
+	@Transactional
+	public Student createStudent(Student s) {
+		return srepo.saveAndFlush(s);
+	}
+
+	@Override
+	@Transactional
+	public Student updateStudent(Student s) {
+		return srepo.saveAndFlush(s);
+	}
+
+	@Override
+	@Transactional
+	public void removeStudent(Student s) {
+		srepo.delete(s);
+	}
+	
+	@Override
+	@Transactional
+	public ArrayList<Student> findStudentsByCriteria(Student student) {
+		return null;
+	}
+	
+	
+	
+
+}
---
+++ StudentSpringDemo/src/main/webapp/WEB-INF/decorators/Layout.jsp
@@ -0,0 +1,30 @@
+<%@ taglib uri="http://www.opensymphony.com/sitemesh/decorator"
+	prefix="dec"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html>
+<head>
+<c:url value="/css/simple.css" var="ss"/>
+<link rel="STYLESHEET" type="text/css"
+	href="${ss}" />
+<dec:head />
+</head>
+<body>
+<jsp:include page="${pageContext.request.contextPath}/decorators/Menu.jsp"/>
+	<table style="width: 100%">
+		<tr>
+			<td>
+				<div>
+					<dec:body />
+				</div>
+			</td>
+		</tr>
+	</table>
+	
+	<!-- ======== Footer ======== -->
+				<div id="footer" align="center">
+				  <hr>
+					<small> &copy; ISS NUS SA42 2016 </small>
+				</div>
+</body>
+</html>
